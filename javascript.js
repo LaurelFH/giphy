daf7096a1043eaa1a5847a4938c407f0d0fae10b
LeeAFH <footehud@gmail.com>
2017-06-26 10:52:48 -0400

    updating layout and general flow

---
+++ javascript.js
@@ -0,0 +1,71 @@
+//javascript for the giphy webpage 
+
+
+
+//global variables
+
+
+
+
+
+
+
+
+//functions 
+
+
+
+
+//display
+
+
+//data
+
+
+
+
+
+
+
+
+
+//events and flow 
+
+
+
+
+
+
+
+
+
+//GENERAL LOGIC 
+
+//USER-SIDE  
+/*
+
+1.  user clicks a button with the label of an animal on it
+2.
+3.
+4.
+5.
+
+
+
+
+//COMPUTER-SIDE (ajax call other notes from readme!)
+
+1.
+2.
+3.
+4.
+5.
+6.
+
+
+
+
+
+
+
+*/
