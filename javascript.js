0b36cc3a9a2cb812cab5bf99a154ffd6d287d442
unknown <footehud@gmail.com>
2017-06-26 18:16:35 -0400

    updating flow notes and layout

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
 //javascript for the giphy webpage 
 
-
+//limit API search to 10
 
 //global variables
-
-
+var animalArray=[];
 
 
 
@@ -12,8 +11,6 @@
 
 
 //functions 
-
-
 
 
 //display
@@ -34,35 +31,30 @@
 
 
 
-
-
-
-
-
-//GENERAL LOGIC 
+//GENERAL LOGIC PLAN 
 
 //USER-SIDE  
 /*
-
-1.  user clicks a button with the label of an animal on it
-2.
-3.
-4.
-5.
+1.  user arrives to the page that has a set of buttons already visible with animal names (to start as a cue? or maybe appended?)
+2.  user clicks a button with one of the animal names on it 
+3.  when clicked, the button calls up 10 gifs which contain the named animal (from the button)
+4.  These 10 gifs from gif's database are visible after the button is clicked
+5. the user can add more buttons by using the "submit" button and a new animal button is created next to the others 
 
 
 
 
 //COMPUTER-SIDE (ajax call other notes from readme!)
 
-1.
-2.
-3.
-4.
-5.
-6.
+1.contain some present cue buttons? when the page is loaded
+2.when the user clicks a pre-set button, then the animal on the button value should be pulled from API
+3. 10 gifs of this animal button's value need to be displayed
+4. the button remains on the page (not refreshed?)
+5. if the user uses the submit/input box to add another animal name, then a new button should be created, with the value from that input box
+6.the button, the gifs from that value and the animal gifs should be made visible 
 
-
+public API key-->  The public beta key is "dc6zaTOxFJmzC”
+https://giphy.api-docs.io/1.0/welcome
 
 
 
